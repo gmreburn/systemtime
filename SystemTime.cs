889536974ecf1d7b00675c28615a6640fcdf1d82
gmreburn <gmreburn+git@gmail.com>
2016-10-11 20:15:52 -0500

    add nuspec update assembly info

---
+++ SystemTime.cs
@@ -0,0 +1,28 @@
+﻿namespace gmreburn
+{
+    using System;
+
+    public static class SystemTime
+    {
+        /// <summary> Normally this is a pass-through to DateTime.Now, but it can be overridden with SetDateTime( .. ) for testing or debugging.
+        /// </summary>
+        public static Func<DateTime> Now = () => DateTime.Now;
+        public static Func<DateTime> UtcNow = () => DateTime.UtcNow;
+
+        /// <summary> Set time to return when SystemTime.Now() is called.
+        /// </summary>
+        public static void SetDateTime(DateTime dateTime)
+        {
+            Now = () => dateTime;
+            UtcNow = () => dateTime;
+        }
+
+        /// <summary> Resets SystemTime.Now() to return DateTime.Now.
+        /// </summary>
+        public static void ResetDateTime()
+        {
+            Now = () => DateTime.Now;
+            UtcNow = () => DateTime.UtcNow;
+        }
+    }
+}
