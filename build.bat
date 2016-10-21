50aacecd403182404521a89864b38cf0a12c8c3d
gmreburn <gmreburn+git@gmail.com>
2016-10-20 20:09:27 -0500

    move build logic to separate file

---
+++ build.bat
@@ -0,0 +1,15 @@
+msbuild /Property:Configuration=net20 /m
+
+msbuild /Property:Configuration=net40 /m
+
+msbuild /Property:Configuration=net45 /m
+
+msbuild /Property:Configuration=net452 /m
+
+msbuild /Property:Configuration=net451 /m
+
+msbuild /Property:Configuration=net46 /m
+
+msbuild /Property:Configuration=net461 /m
+
+NuGet pack SystemTime/SystemTime.csproj -NonInteractive
